c1e52473f42fc58482ecb272982230dbc2901554
jc568947953 <568947953@qq.com>
2019-01-23 17:32:00 +0800

    添加好友js

--- src/main/webapp/resource/js/addfriend.js
+++ src/main/webapp/resource/js/addfriend.js
@@ -1,14 +1,15 @@
  $("#add_friend").click(function () {
-    var nickname="张三";
+    var username="qwe123";
     var permisssion_type=1;
+    var note="请问加个好友可以嘛";
     $.ajax({
         type: "post",
         async: false,
-        url: "/friend/addFriend/"+ nickname,
-        data:{"permisssion_type":permisssion_type},
+        url: "/friend/addFriend/"+ username,
+        data:{"permisssion_type":permisssion_type,"note":note},
         dataType: "json",
         success: function (data) {
-            var question = data.nickName;
+            var question = data.username;
         }
     });
 });
