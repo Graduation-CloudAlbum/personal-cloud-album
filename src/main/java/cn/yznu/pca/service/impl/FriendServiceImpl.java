791cc49364cdae420eb2913dcecac7746215f9c8
jc568947953 <568947953@qq.com>
2019-01-23 15:12:36 +0800

    好友serviceimpl

--- src/main/java/cn/yznu/pca/service/impl/FriendServiceImpl.java
+++ src/main/java/cn/yznu/pca/service/impl/FriendServiceImpl.java
@@ -1,11 +1,13 @@
 package cn.yznu.pca.service.impl;
 
 import cn.yznu.pca.dao.PermissionGroupMapper;
+import cn.yznu.pca.dao.UserMapper;
 import cn.yznu.pca.dao.UserRelationMapper;
 import cn.yznu.pca.model.PermissionGroup;
 import cn.yznu.pca.model.User;
 import cn.yznu.pca.model.UserRelation;
 import cn.yznu.pca.model.example.PermissionGroupExample;
+import cn.yznu.pca.model.example.UserExample;
 import cn.yznu.pca.model.example.UserRelationExample;
 import cn.yznu.pca.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,10 @@ public class FriendServiceImpl implements FriendService {
 
     @Autowired
     UserRelationMapper userRelationMapper;
+
+    @Autowired
+    UserMapper userMapper;
+
     @Override
     public List<?> selectFriendGroup(User user) {
         PermissionGroupExample permissionGroupExample=new PermissionGroupExample();
@@ -84,4 +90,32 @@ public class FriendServiceImpl implements FriendService {
         List<?> list = userRelationMapper.selectByExample(userRelationExample);
         return list;
     }
+
+    @Override
+    public void addFriend(User user, int user_two,int permisssion_type) {
+        UserRelation userRelation=new UserRelation();
+        UserRelation userRelation2=new UserRelation();
+        userRelation.setUserId(user.getId());
+        userRelation.setUserIdTwo(user_two);
+        userRelation.setStatus("0");
+        userRelation.setPermissionGroupId(1);
+        userRelation.setPermissionGroupIdTwo(1);
+        userRelation2.setUserId(user_two);
+        userRelation2.setUserIdTwo(user.getId());
+        userRelation2.setStatus("0");
+        userRelation2.setPermissionGroupId(permisssion_type);
+        userRelation2.setPermissionGroupIdTwo(1);
+        userRelationMapper.insert(userRelation);
+        userRelationMapper.insert(userRelation2);
+    }
+
+    @Override
+    public List<User> selectFriendByUsername(String username) {
+        UserExample userExample=new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+        criteria.andNickNameEqualTo(username);
+        List<User> list=userMapper.selectByExample(userExample);
+        return list;
+    }
+
 }
