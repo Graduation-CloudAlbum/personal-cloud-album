1eca3cb65d70f610f5ad0be99ef44cd3393f8516
jc568947953 <568947953@qq.com>
2019-01-26 11:25:11 +0800

    添加好友验证外键约束

--- src/main/webapp/resource/js/login.js
+++ src/main/webapp/resource/js/login.js
@@ -3,12 +3,12 @@ $("#login_btn").click(function(){
         var password = $.trim($("#password").val());
         $.ajax({
             type:"post",
-            url:"/user/checkLogin",
+            url:"/pca/user/checkLogin",
             data:{"username":username,"password":password},
             dataType: "json",
             complete:function(result) {
                 if (result.responseText=="success") {
-                    window.location.href="/user/myAlbum";
+                    window.location.href="/pca/user/myAlbum";
                 } else {
                     alert("用户名或密码错误 请重试");
                     $("#username").val("");
