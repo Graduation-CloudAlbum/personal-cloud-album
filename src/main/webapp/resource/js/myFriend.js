e039fdaf2f1b8b7e803cf363e7826c8fc9aeb034
devyangbw <248795961@qq.com>
2019-02-21 09:59:48 +0800

    friend

--- src/main/webapp/resource/js/myFriend.js
+++ src/main/webapp/resource/js/myFriend.js
@@ -1,23 +1,23 @@
 function selectOnde(permissionType) {
 
     alert(permissionType);
-    if (permissionType==0){
-        url="/pca/friend/selectMyFriend";
-        alert(url);
-    }else if (permissionType==1){
-        url="/pca/friend/selectMyFamily"
-        alert(url);
-    } else if (permissionType==2){
-        url="/pca/friend/selectMyClassmate";
-        alert(url);
-    } else {
-        url="/pca/friend/selectMyStranger";
-        alert(url);
-    }
+    // if (permissionType==0){
+    //     url="/pca/friend/selectMyFriend";
+    //     alert(url);
+    // }else if (permissionType==1){
+    //     url="/pca/friend/selectMyFamily"
+    //     alert(url);
+    // } else if (permissionType==2){
+    //     url="/pca/friend/selectMyClassmate";
+    //     alert(url);
+    // } else {
+    //     url="/pca/friend/selectMyStranger";
+    //     alert(url);
+    // }
     $.ajax({
         type:"POST",
-        url:url,
-
+        // url:url,
+        url:"/pca/friend"+"/"+permissionType,
         data:{"modifyingData":permissionType},
         dataType: "json",
         complete:function(result) {
