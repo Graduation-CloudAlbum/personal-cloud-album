a134921455cea937498edbd779b325ade8116e7c
jc568947953 <568947953@qq.com>
2019-03-10 23:28:08 +0800

    显示回收站

--- src/main/java/cn/yznu/pca/service/impl/RecycleBinServiceImpl.java
+++ src/main/java/cn/yznu/pca/service/impl/RecycleBinServiceImpl.java
@@ -1,6 +1,13 @@
 package cn.yznu.pca.service.impl;
 
+import cn.yznu.pca.dao.RecycleBinMapper;
+import cn.yznu.pca.dao.UserRelationMapper;
+import cn.yznu.pca.model.RecycleBin;
+import cn.yznu.pca.model.example.PermissionGroupExample;
+import cn.yznu.pca.model.example.RecycleBinExample;
 import cn.yznu.pca.service.RecycleBinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -8,10 +15,14 @@ import java.util.List;
  * @author yangbaiwan
  * @date 2019-03-04
  */
+@Service
 public class RecycleBinServiceImpl implements RecycleBinService {
+    @Autowired
+    RecycleBinMapper recycleBinMapper;
     @Override
-    public List getRecycleBin(int userId) {
-        return null;
+    public List<RecycleBin> getRecycleBin(int userId) {
+        List <RecycleBin> recycleBins=recycleBinMapper.selectMyRecycleBin(userId);
+        return recycleBins;
     }
 
     @Override
