ed64fa020fb062560bb5fae1244c515811274715
jc568947953 <568947953@qq.com>
2019-03-22 14:06:42 +0800

    login();方式修改

--- src/main/webapp/resource/js/login.js
+++ src/main/webapp/resource/js/login.js
@@ -1,21 +1,41 @@
-function login(){
-        var username = $.trim($("#i-input-email").val());
-        var password = $.trim($("#i-input-password").val());
-
+// function login(){
+//         var username = $.trim($("#i-input-email").val());
+//         var password = $.trim($("#i-input-password").val());
+//
+//     $.ajax({
+//             async : false,
+//             type:"post",
+//             url:"/pca/user/checkLogin",
+//             data:{"username":username,"password":password},
+//             dataType: "json",
+//             complete:function(result) {
+//                 if (result.responseText=="success") {
+//                     window.location.href="/pca/user/myAlbum";
+//                 }else {
+//                     alert("用户名或密码错误 请重试");
+//                     $("#username").val("");
+//                     $("#password").val("");
+//                 }
+//             }
+//         });
+// }
+$("#loginButton").click(function () {
+    var username = $.trim($("#i-input-email").val());
+    var password = $.trim($("#i-input-password").val());
     $.ajax({
-            async : false,
-            type:"post",
-            url:"/pca/user/checkLogin",
-            data:{"username":username,"password":password},
-            dataType: "json",
-            complete:function(result) {
-                if (result.responseText=="success") {
-                    window.location.href="/pca/user/myAlbum";
-                }else {
-                    alert("用户名或密码错误 请重试");
-                    $("#username").val("");
-                    $("#password").val("");
-                }
+        async : false,
+        type:"post",
+        url:"/pca/user/checkLogin",
+        data:{"username":username,"password":password},
+        dataType: "json",
+        complete:function(result) {
+            if (result.responseText=="success") {
+                window.location.href="/pca/user/myAlbum";
+            }else {
+                alert("用户名或密码错误 请重试");
+                $("#username").val("");
+                $("#password").val("");
             }
-        });
-}
+        }
+    });
+});
